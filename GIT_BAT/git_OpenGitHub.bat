7ee69ddc1b436262078048fa76a914ae8ac02ddd
walter <47453186+icivixwalter@users.noreply.github.com>
2023-10-20 13:56:52 +0200

    agg

--- GIT_BAT/git_OpenGitHub.bat
+++ GIT_BAT/git_OpenGitHub.bat
@@ -13,11 +13,20 @@ start https://github.com/icivixwalter
 
 
 
-goto errori_git
+goto utilita
 
-C 
-	COMMIT
-			nothing to commit, working tree clean
-				niente da impegnare, albero funzionante pulito
 
-:errori_git
+PATH_APRI_ERRORI_GIT
+	Path e file per la gestione degli errori di git che si trova nel file omonino.
+
+	@errori@git_(aprire il file con start per gestire gli errori di git)
+		Start "apri il file" C:\CASA\LINGUAGGI\GIT_DESKTOP\Errori_Git\Errori_Git.md
+
+	PATH_TUTORIAL_GIT
+		Per aprire il file tutorial di git per i vari comandi
+			@comandi@github_(si trovano in questo file)
+				
+				Start "apri il file" C:\CASA\LINGUAGGI\GIT_DESKTOP\Errori_Git\Errori_Git.md
+
+
+:utilita
