a292b375ad2e2244faf3f82729c635a8150d0aaf
walter <47453186+icivixwalter@users.noreply.github.com>
2023-10-20 12:03:10 +0200

    agg

--- GIT_BAT/git_OpenGitHub.bat
+++ GIT_BAT/git_OpenGitHub.bat
@@ -1,2 +1,23 @@
 
 start https://github.com/icivixwalter
+
+
+
+
+
+
+
+
+
+
+
+
+
+goto errori_git
+
+C 
+	COMMIT
+			nothing to commit, working tree clean
+				niente da impegnare, albero funzionante pulito
+
+:errori_git
