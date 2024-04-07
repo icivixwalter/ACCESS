9e00af67731dba19997247d7b2da67e260917b82
icivixwalter <icivixwalter@gmail.com>
2024-04-07 15:16:04 +0200

    risistemazione access

--- ACCESS_PROGETTI_MDB/ACCESS_GE_OGGETTI_NEW/OPEN_ANALISI_SublimeText.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-@REM		directory y dove archiviare i dati = path di destinazione
-@REM .......................................................
-SET PATH_DEST_S=c:\CASA\LINGUAGGI\ACCESS\ACCESS_PROGETTI_MDB\ACCESS_GE_OGGETTI_NEW\GE_OGGETTI_NEW_ANALISI\Analisi_SublimeText\
-SET FILE_OPEN_s=MSys_OGGETTI.sublime-project
-start "apri file" %PATH_DEST_S%%FILE_OPEN_s% &^exit
--- ACCESS_PROGETTI_MDB/ACCESS_GE_OGGETTI_NEW/OPEN_MDB.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-@REM		directory y dove archiviare i dati = path di destinazione
-@REM .......................................................
-SET PATH_DEST_S=c:\CASA\LINGUAGGI\ACCESS\ACCESS_PROGETTI_MDB\ACCESS_GE_OGGETTI_NEW\GE_OGGETTI_NEW_MDB\
-SET FILE_OPEN_s=GE_OGGETTI_Access_2000_NEW.mdb
-start "apri file" %PATH_DEST_S%%FILE_OPEN_s% &^exit
